dfbc89cea2a7aba80e12395924f341007bf2ff47
Htetko510217 <htetko510217@gmai.com>
2024-08-02 17:08:56 +0700

    feat : add filament resource for product and order

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -9,4 +9,9 @@ class Order extends Model
 {
     use HasFactory;
     protected $fillable = ['user_id', 'total', 'status'];
+
+     public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
